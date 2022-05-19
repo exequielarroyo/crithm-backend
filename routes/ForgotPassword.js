100ac883e1eb1af99c04cde8714ddf140c17f7a9
Exequiel Arroyo <zekielarroyo@gmail.com>
2022-05-19 12:10:10 +0800

    added nodemailer & implement forgot password email

--- routes/ForgotPassword.js
+++ routes/ForgotPassword.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const { sign, verify } = require("jsonwebtoken");
 const { User } = require("../models");
 const bcrypt = require("bcrypt");
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({service: 'gmail', auth: {user:'crithm.cf@gmail.com', pass: 'abcd_1234'}});
 
-router.get("/forgot",  (req, res, next) => {});
+
 router.post("/forgot", async (req, res, next) => {
   const { email } = req.body;
   const user = await User.findOne({ where: { email } });
@@ -17,6 +19,19 @@ router.post("/forgot", async (req, res, next) => {
 
   // res.json("Password reset link has been sent");
   // TODO send link to email
+  var mailOptions = {
+    from: "crithm.cf@gmail.com",
+    to: user.email,
+    subject: "Sending Email using Node.js",
+    text: link,
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  })
   res.json(link);
 });
 
@@ -52,7 +67,7 @@ router.post("/reset/:id/:token", async(req, res, next) => {
     else {
       const email = decoded.email;
       bcrypt.hash(password, 10).then(async (hashed) => {
-        await User.update({ ...user, password: hashed }, { where: { email: user.email } });
+        await User.update({ ...user, password: hashed }, { where: { email } });
         res.json('password changed')
       });
     }
